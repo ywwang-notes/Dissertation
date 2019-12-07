f26765f1265822b85bc576797e0196c8efd177c1
ywwang-notes <51767154+ywwang-notes@users.noreply.github.com>
2019-12-07 08:31:38 -0800

    group normalization (other than betas)

--- group_norm_other.m
+++ group_norm_other.m
@@ -9,12 +9,17 @@ upperdir = '/mnt/Work/SystemSwitch/Analysis/'; % revise here
 clear matlabbatch;
 matlabbatch{1}.cfg_basicio.run_ops.runjobs.jobs = {[upperdir 'wbeta.m']}; % revise here
 
-sidlst = [0004 0567 0679 0739 0844 1061 1091 1205 1676 1697 1710 1886 1993 ...
-    2010 2054 2055 2099 2187 2372 2809 3008 3431 3552 3883 3973 4320];
-prefix = 'norm';
-sidlst = [0003];
+sidlst = [0001 0002 0003 0004 0567 0679 0739 0844 0893 ...
+    1000 1061 1091 1205 1676 1697 1710 1886 1993 ...
+    2010 2054 2055 2099 2167 2187 2372 2526 2764 2809 ...
+    3008 3034 3080 3149 3431 3461 3552 3883 3973 ...
+    4087 4289 4298 4320 4599 4765 4958];
+prefix = 'wsvm';
 contents = dir('SVM');
 n = length(contents);
+n_batch = 0;
+
+w_sid = [];
 
 for sbj=1:length(sidlst) % revise here
     sid = num2str(sidlst(sbj), '%04i');
@@ -26,11 +31,17 @@ for sbj=1:length(sidlst) % revise here
             filelist{end+1,1} = ['SVM/' contents(i).name ',1'];
         end
     end
+    
+    if isempty(filelist)
+        continue;
+    end
 
-matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{sbj}{1}.innifti = {[upperdir sid '/GLM/t2.nii,1']};
-matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{sbj}{2}.innifti = {[upperdir sid '/GLM/mean.nii,1']};
-matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{sbj}{3}.innifti = filelist;
-matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{sbj}{4}.inany = {[upperdir sid '/GLM/y_rt1.nii']};
+    n_batch = n_batch + 1;
+    w_sid = [w_sid sidlst(sbj)];
+    matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{n_batch}{1}.innifti = {[upperdir sid '/GLM/t2.nii,1']};
+    matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{n_batch}{2}.innifti = {[upperdir sid '/GLM/mean.nii,1']};
+    matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{n_batch}{3}.innifti = filelist;
+    matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{n_batch}{4}.inany = {[upperdir sid '/GLM/y_rt1.nii']};
 
 end
 
@@ -39,3 +50,10 @@ matlabbatch{1}.cfg_basicio.run_ops.runjobs.save.savejobs.outdir = {upperdir};
 matlabbatch{1}.cfg_basicio.run_ops.runjobs.missing = 'skip';
 
 spm_jobman('run',matlabbatch);
+
+sidlst = w_sid;
+
+for sbj=1:length(w_sid)
+   fname = sprintf('%s_%03i.m', prefix, sbj);
+   movefile(fname, sprintf('SVM/for_debug/%s_%04i.m', prefix, w_sid(sbj)));    
+end
