be75365fc04d431124aaab38c63207eeb709a1ff
ywwang-notes <yiwen@ucsb.edu>
2020-03-07 11:52:35 -0800

    backup: MacOS upgrade

---
+++ count_f.sh
@@ -0,0 +1,50 @@
+current=$(pwd)
+
+cd ~/Documents/GoogleDrive/SystemSwitch/UP2Smoothed/
+for subject in *
+do
+
+        if [ ! -d "$subject" ]; 
+	then
+#        	echo $subject not a folder
+                continue
+        fi
+	cd $subject
+
+	echo $subject
+
+	for folder in cmrr*
+	do 
+		if [ ! -d $folder ]; then
+			continue
+		fi
+
+		cd $folder
+
+		# do not write: echo *.nii
+		# instead, write: echo "*.nii"
+		# lack of quote marks will force ls output to std
+
+		# no space before and after `=' !!!
+		f=$(ls| grep ^f.*nii |wc -l)
+		uf=$(ls| grep ^uf.*nii |wc -l)
+		suf=$(ls| grep ^suf.*nii |wc -l)
+
+		if [ $f != $uf ] || [ $f != $suf ];
+		then
+			echo $folder
+			echo -e "\t f.nii \t $f"
+			echo -e "\t uf.nii \t $uf"
+			echo -e "\t suf.nii \t $suf"
+		else 
+			echo -e "$folder complete $uf"
+		fi
+
+		cd ..
+	done
+
+	cd ..
+
+done
+
+cd $current
