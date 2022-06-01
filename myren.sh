7cba15d0795877ece49d2e223fe0256387fedf72
ywwang-notes <ywwang.studio@gmail.com>
2022-06-01 15:30:39 +0800

    Update myren.sh

--- myren.sh
+++ myren.sh
@@ -1,7 +1,8 @@
 # usage: in the target folder, type: myren "old_sub_string" "new_sub_string"
 # whitespace is OK
 
-for f in *"$1"*; do
+for f in *"$1"*; do 
     newname=${f/"$1"/"$2"}
-    mv -v $f $newname
+#    echo $newname
+    mv -v "$f" "$newname"
 done
